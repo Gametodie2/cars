240c24fc1299ec6cfbd02736c16a30ba59c9a076
Yani <yani.degrande@gmail.com>
2025-07-17 20:20:28 +0200

    road

--- car/main.js
+++ car/main.js
@@ -2,6 +2,7 @@ const carCanvas = document.getElementById("carCanvas");
 carCanvas.width = 200;
 
 const carCtx = carCanvas.getContext("2d");
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const car = new Car(100, 100, 30, 50);
 car.draw(carCtx);
 
@@ -9,8 +10,15 @@ animate();
 
 function animate() {
   car.update();
+
   carCanvas.height = window.innerHeight;
 
+  carCtx.save();
+  carCtx.translate(0, -car.y + carCanvas.height * 0.7);
+
+  road.draw(carCtx);
   car.draw(carCtx);
+
+  carCtx.restore();
   requestAnimationFrame(animate);
 }
