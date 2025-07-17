72f1f993c759f0447c7d27710699bfd6480fc742
Yani <yani.degrande@gmail.com>
2025-07-17 23:03:30 +0200

    collision detection

--- car/main.js
+++ car/main.js
@@ -4,7 +4,6 @@ carCanvas.width = 200;
 const carCtx = carCanvas.getContext("2d");
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const car = new Car(100, 100, 30, 50);
-car.draw(carCtx);
 
 animate();
 
@@ -15,6 +14,8 @@ function animate() {
 
   carCtx.save();
   carCtx.translate(0, -car.y + carCanvas.height * 0.7);
+
+  car.draw(carCtx);
 
   road.draw(carCtx);
   car.draw(carCtx);
