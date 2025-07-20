1ad05bc1f093e754312dfa5f0396a34bf2aba71c
Yani <yani.degrande@gmail.com>
2025-07-20 14:45:03 +0200

    grab and pan + start roads

--- world/js/math/utils.js
+++ world/js/math/utils.js
@@ -14,3 +14,26 @@ function getNearestPoint(loc, points, threshold = Number.MAX_SAFE_INTEGER) {
 function distance(p1, p2) {
   return Math.hypot(p1.x - p2.x, p1.y - p2.y);
 }
+
+function add(a, b) {
+  return new Point(a.x + b.x, a.y + b.y);
+}
+
+function substract(a, b) {
+  return new Point(a.x - b.x, a.y - b.y);
+}
+
+function scale(p, scaler) {
+  return new Point(p.x * scaler, p.y * scaler);
+}
+
+function translate(point, angle, offset) {
+  return new Point(
+    point.x + Math.cos(angle) * offset,
+    point.y + Math.sin(angle) * offset
+  );
+}
+
+function angle(p) {
+  return Math.atan2(p.y, p.x);
+}
